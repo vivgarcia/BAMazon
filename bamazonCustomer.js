d6c5fac98d9178f5906336ac230b2d90cfb44e7e
vivgarcia <viv.garcia88@gmail.com>
2020-04-20 15:02:27 -0500

    updated code after tutoring

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,40 +29,44 @@ function start() {
         console.log("ID: " + res[i].id + "\nProduct: " + res[i].product_name + "\nDepartment: " 
         + res[i].department_name + "\nPrice: " + res[i].price + "\nQty: " + res[i].stock_quantity + "\n----------------------------------");
     }
-    connection.end();
-    });
+    
+
 
     inquirer.prompt([
         {
-            type: "input",
+            type: "list",
             name: "id",
             message: "What is the ID of the product you'd like to purchase?",
-            validate: function(value){
-                if(isNaN(value) === false && parseInt(value) <= res.length && parseInt(value) > 0){
-                    return true; 
-                }else{
-                    return false;
+            choices: function(){
+                let idChoices = [];
+
+                for(var i = 0; i < res.length; i++){
+                    idChoices.push(res[i].id);
                 }
+                return idChoices; 
             }
         },
         {
             type: "input",
             name: "qty",
-            message: "How much would you like to purchase?",
-            validate: function(value){
-                if(isNaN(value)){
-                    return false;
-                } else{
-                    return true;
-                }
-            }
+            message: "How much would you like to purchase?"
         }
     ]).then(function(answer){
-        let itemBought = (answer.id)-1;
-        let qtyBought = parseInt(ans.qty);
-        let total = parseFloat(((res[itemBought].price) * qtyBought).toFixed(2));
-    
+        let itemBought = answer.id - 1;
+        let qtyBought = parseInt(answer.qty);
+        let total = res[itemBought].price * qtyBought.toFixed(2);
+        console.log(total);
+        console.log(res[itemBought].stock_quantity);
+        if(res.stock_quantity > qtyBought){
+            connection.query("UPDATE products SET ? WHERE ?", [
+                {stock_quantity: res[itemBought].stock_quantity - qtyBought},
+                {id: answer.id}
+            ],
+            )
+        }
+        console.log("Success! Your total is $" + total.toFixed(2) + ". Your item(s) will arrive shortly!");
+        connection.end();
     })
 
-    
+});   
 }
